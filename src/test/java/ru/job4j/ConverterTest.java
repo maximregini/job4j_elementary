0f106c8d08ff1fef4eb791c753b2b6c89c44875c
MAFIOSO <2821usm3@gmail.com>
2021-12-20 20:39:26 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert420rblThen6Eur() {
+        float in = 420;
+        float expected = 6;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert240rblThen4Dlr () {
+        float in = 240;
+        float expected = 4;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
