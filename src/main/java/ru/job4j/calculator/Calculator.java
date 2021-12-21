98cab71dcbdf44b4a4032be264b345b768d2f714
MAFIOSO <2821usm3@gmail.com>
2021-12-21 09:48:25 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,20 +1,29 @@
 package ru.job4j.calculator;
 
+import static ru.job4j.math.MathFunction.*;
+
 public class Calculator {
 
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double sumSubAndDiv(double first, double second) {
+        return subtract(first, second)
+                + divide(first, second);
+    }
+
+    public static double sumAll(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second)
+                + subtract(first, second)
+                + divide(first, second);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int onePlusTwo = one + two;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
-        System.out.println("sixDivTwo = " + sixDivTwo);
-        System.out.println("fiveMinusTwo = " + fiveMinusTwo);
-        System.out.println("fourTimeTwo = " + fourTimeTwo);
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + sumSubAndDiv(10, 20));
+        System.out.println("Результат расчета равен: " + sumAll(10, 20));
     }
 }
