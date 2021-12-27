e9ff61ba067f38920aad05ff981db23d3150ce72
MAFIOSO <2821usm3@gmail.com>
2021-12-27 21:48:23 +0300

    4.6.1. Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        String name = switch (day) {
+            case 1 -> "Понедельник";
+            case 2 -> "Вторник";
+            case 3 -> "Среда";
+            case 4 -> "Четверг";
+            case 5 -> "Пятница";
+            case 6 -> "Суббота";
+            case 7 -> "Воскресенье";
+            default -> "Ошибка";
+        };
+        return name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nameOfDay(4));
+    }
+}
