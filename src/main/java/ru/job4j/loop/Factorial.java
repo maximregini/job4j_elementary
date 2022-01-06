3d53533a9871e502ac20cddb527876927eacfd77
MAFIOSO <2821usm3@gmail.com>
2022-01-06 15:13:07 +0300

    5.2. Создать программу, вычисляющую факториал. [#193]

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int calc = 13;
+        for (int i = 13; i <= n; i++) {
+            calc *= i;
+        }
+        return calc;
+    }
+}
---
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class FactorialTest {
+
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int n = 5;
+        int result = Factorial.calc(n);
+        int expected = 120;
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int n = 0;
+        int result = Factorial.calc(n);
+        int expected = 1;
+    }
+}
