cd78c407d7d878fe857d111b5a3c32c717a3da89
MAFIOSO <2821usm3@gmail.com>
2022-01-07 17:58:50 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount >= salary) {
+            amount = amount + amount * percent / 100 - salary;
+            year++;
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(year(250, 400, 50));
+    }
+}
