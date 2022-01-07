da904bcd400dfdd9c1a6e2b3996e515ea79a576c
MAFIOSO <2821usm3@gmail.com>
2022-01-07 16:04:07 +0300

    5.5. Простое число [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        for (int index = 2; index < number; index++) {
+            if (number % index == 0) {
+                return false;
+            }
+        }
+        return prime;
+    }
+}
