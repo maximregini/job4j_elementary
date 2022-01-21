b989af0c824b0e18ecf3d828de041487f573498c
MAFIOSO <2821usm3@gmail.com>
2022-01-21 21:40:06 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        System.out.println(names[0]);
+        names[1] = "Maxim Regini";
+        System.out.println(names[1]);
+        names[2] = "Olga Nuzhdina";
+        System.out.println(names[2]);
+        names[3] = "Kirill Mesheryakov";
+        System.out.println(names[3]);
     }
 }
