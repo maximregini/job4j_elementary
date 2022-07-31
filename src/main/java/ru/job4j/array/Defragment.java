618c9b4fd8755b115b715d59e2a4a7a672088b95
MaximRegini <maxadenk@mail.ru>
2022-07-31 18:46:28 +0300

    6.8. Дефрагментация массива [#225 #327223]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -5,7 +5,7 @@ public class Defragment {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
                 /* переместить первую не null ячейку. Нужен цикл. */
-                for (int i = index; i < array.length; i++) {
+                for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
                         array[index] = array[i];
                         array[i] = null;
